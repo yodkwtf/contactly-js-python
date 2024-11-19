98c70a4d632b304cb20e0274c593e0a2e0d64628
yodkwtf <48durgesh.kumar@gmail.com>
2024-11-19 23:58:06 +0530

    basic python function and for loop

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
-print("Hello from Contactly!")
+def my_func(name):
+    if name == "Deekayy":
+        print("Hello Deekayy")
+    else:
+        print("Hello Stranger")
+
+    for i in range(3):
+        print(i + 1, " seconds has passed")
+
+
+my_func("Deekayy")
+my_func("John")
